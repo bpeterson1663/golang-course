d06d1b85a092146dc5acd0c940f639cb1b79229d
Brady Peterson <bpeterson1663@gmail.com>
2021-07-06 15:29:54 -0500

    feat(intro): working with for loop

--- Hello-World-Go/main.go
+++ Hello-World-Go/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"myApp/doctor"
+	"os"
+	"strings"
 )
 
 func main() {
-	var whatToSay string
-	whatToSay = doctor.Intro()
-	// whatToSay := "Hellow world as an argument" //shorthand to infer type
-	// sayHelloWorld(whatToSay)
+	reader := bufio.NewReader(os.Stdin)
+	whatToSay := doctor.Intro()
 	fmt.Println(whatToSay)
-}
 
-// func sayHelloWorld(whatToSay string) {
-// 	fmt.Println(whatToSay)
-// }
+	for { //only what type of loop
+		fmt.Print("-> ")
+		userInput, _ := reader.ReadString('\n')
+
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == "quit" {
+			break
+		} else {
+			fmt.Println(doctor.Response(userInput))
+		}
+	}
+
+}
