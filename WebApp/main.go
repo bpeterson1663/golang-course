ef0481646ed122b8dcbb0ea803491f892b5a68b5
Brady Peterson <bpeterson1663@gmail.com>
2021-08-01 19:33:25 -0500

    feat(web app): serving html files

--- WebApp/main.go
+++ WebApp/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world")
+	renderTemplate(w, "index.html")
 }
 
 func main() {
 	http.HandleFunc("/", homePage)
 	log.Println("Starting web server on port 8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+func renderTemplate(w http.ResponseWriter, page string) {
+	t, err := template.ParseFiles(page)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = t.Execute(w, nil)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
