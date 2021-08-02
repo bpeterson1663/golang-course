b1e7ca43f5990bf680f825ef327645bc9b500ce1
Brady Peterson <bpeterson1663@gmail.com>
2021-08-01 19:47:29 -0500

    feat(web app): parse and send json results

--- WebApp/main.go
+++ WebApp/main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
+	"webapp/rps"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "index.html")
 }
 
+func playRound(w http.ResponseWriter, r *http.Request) {
+	result := rps.PlayRound(1)
+	log.Println(result)
+
+	out, err := json.MarshalIndent(result, "", "   ")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+}
+
 func main() {
+	http.HandleFunc("/play", playRound)
 	http.HandleFunc("/", homePage)
 	log.Println("Starting web server on port 8080")
 	http.ListenAndServe(":8080", nil)
